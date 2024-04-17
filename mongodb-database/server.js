acc0a7f6ac2b487fc68b37ff493e39836e29c3ab
xfekke <fekke4201337@gmail.com>
2024-04-17 14:20:07 +0200

    Added rate limiting

--- mongodb-database/server.js
+++ mongodb-database/server.js
@@ -1,13 +1,22 @@
 // Importera Express för att kunna skapa en webbserver och Mongoose för att interagera med MongoDB-databasen.
 import express from "express"
 import mongoose from "mongoose"
+import { rateLimit } from 'express-rate-limit';
 import apiRegister from "./apiRegister.js"
 
 // Skapar en instans av Express-appen, detta är vår webbserver.
 const server = express()
-
-// Bestämmer vilken port som servern ska lyssna på.
 const port = 3000
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
+server.use(limiter);
 
 /*
   Servern använder en middleware ( express.json() ) för att omvandla våra request till JSON.
